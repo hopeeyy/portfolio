5cc48efaf34c1ac8290b9f0d4c46507bdae8532c
hopeeyy <hopeeyy.13@gmail.com>
2017-07-18 16:01:01 -0400

    shapes.py

---
+++ shapes.py
@@ -0,0 +1,24 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = -0
+y_pos = -150
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+forward(150)
+right(90)
+forward(150)
+right (90)
+forward (150)
+right(110)
+forward(150)
+right(125)
+forward(170)
+# Close window on click.
+exitonclick()
